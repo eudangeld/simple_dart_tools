c07d31cdc98f63f93a7c60bb7422d94694849c38
dan <eudangeld@gmail.com>
2020-10-06 09:38:02 -0300

    removing new expression

--- lib/dart_tools.dart
+++ lib/dart_tools.dart
@@ -11,7 +11,7 @@ class DartValidators {
     String emailRegex =
         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
 
-    RegExp regExp = new RegExp(emailRegex);
+    RegExp regExp = RegExp(emailRegex);
 
     return regExp.hasMatch(em);
   }
